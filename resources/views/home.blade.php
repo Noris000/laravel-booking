5e16d0be1d6e725e9266ffbaa1a030541d515817
Normunds Lazdins <normundslazdins16@gmail.com>
2023-11-27 13:04:53 +0200

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,27 @@
 
 @section('content')
 <div class="container">
-    @foreach ($rooms as $room)
-        <div class="card">
-        <p>{{$room->room_nr}}
-            {{$room->name}}
-            {{$room->description}}
-            {{$room->price}}
-        </p>
-        </div>
-        
-    @endforeach
+    <h1>Available Rooms</h1>
+    <div class="row">
+        @foreach ($rooms as $room)
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="{{ $room->image_url }}" class="card-img-top" alt="{{ $room->name }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $room->name }}</h5>
+                        <p class="card-text">{{ $room->description }}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Room Number: {{ $room->room_nr }}</li>
+                        <li class="list-group-item">Price: ${{ $room->price }}</li>
+                    </ul>
+                    <div class="card-body">
+                        <!-- You can add booking button or form here -->
+                        <a href="#" class="btn btn-primary">Book Now</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </div>
 @endsection
